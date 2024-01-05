b326bd8481f0d1b719b7b4c8b80ebc234db75d46
Fernando Gómez <141048801+Axus00@users.noreply.github.com>
2024-01-04 20:51:30 -0500

    Add files via upload

---
+++ github.js
@@ -0,0 +1,18 @@
+//Solicitud de información para perfil de github
+const username = "Axus00";
+const token = "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";
+
+fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
+.then(response => response.json())
+.then(data => {
+    document.getElementById("avatar").src = data.avatar_url;
+    document.getElementById("username").textContent = `Usuario: ${data.login}`;
+    document.getElementById("bio").textContent = `Biografía: ${data.bio}`;
+    document.getElementById("followers").textContent = `Seguidores: ${data.followers}`;
+    document.getElementById("repos").textContent = `Repositorios públicos: ${data.public_repos}`;
+})
+.catch(error => console.error('Error al recuperar datos: ', error));
