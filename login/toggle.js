2cb9e1a8c9087ecdae3601daa5a7410a7643804b
Fernando Gómez <141048801+Axus00@users.noreply.github.com>
2024-01-04 19:58:38 -0500

    Delete login directory

--- login/toggle.js
+++
@@ -1,7 +0,0 @@
-let toggle = document.getElementById('images');
-let body = document.querySelector('body');
-
-toggle.onclick = function(){
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
-}
